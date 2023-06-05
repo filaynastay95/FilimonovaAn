5326232767d7e2141e5385f1fb111af70efb49af
filaynastay95 <filay.nastay95@gmail.com>
2023-06-05 19:02:55 +0300

    working with variables

---
+++ les1.py
@@ -0,0 +1,49 @@
+"""#Работа со сменными с урока
+zminna1 = input('Введить число')
+zminna2 = input('Введить друге число')
+result = int(zminna1) + int(zminna2)
+print ('A+B=',result)"""
+
+"""zminna1 = input('Введить число A ')
+zminna2 = input('Введить число B ')
+zminna3 = input('Введить число C ')
+result1 = float(zminna1) + float(zminna2) + float(zminna3)
+result2 = float(zminna1) * float(zminna2) * float(zminna3)
+print ('A+B+С=',result1,'\nA*B*C=',result2)"""
+
+"""zminna1 = input('Введить Ваш дохід за поточний місяц: ')
+zminna2 = input('Введить суму регулярного платежу за кредтом: ')
+zminna3 = input('Введить поточну заборгованість за комунальні послуги: ')
+result1 = float(zminna1) - float(zminna2) - float(zminna3)
+print ('Остаток вільних коштів складає :',result1,'грн.')"""
+
+"""zminna1 = input('Задайте значення діагоналі ромба d1: ')
+zminna2 = input('Задайте значення діагоналі ромба d2: ')
+result1 = float(zminna1) * float(zminna2)
+print ('Площа ромба с заданими діагоналями дорівнює:',result1/2)"""
+
+"""print(' '*5,' To be\n',' '*5,'or not\n', ' '*5,'to be')"""
+
+"""print('«Life is what happens\n',' '*8,'when\n',' '*11,' you’re busy making other plans.»\n',' '*32,' John Lennon.')"""
+
+"""print('«Life is what happens')
+print(' '*12,'when')
+print(' '*10,'you’re busy making other plans.»')
+print(' '*32,' John Lennon.')"""
+
+"""zminna1 = input('Введить Ваш дохід за поточний місяц: ')
+zminna2 = input('Введить суму регулярного платежу за кредтом: ')
+zminna3 = input('Введить поточну заборгованість за комунальні послуги: ')
+result1 = float(zminna1) - float(zminna2) - float(zminna3)
+result2= int(result1 *100)/100
+print ('Остаток вільних коштів складає :',result2,'грн.')"""
+
+"""zminna1 = input('Введить Ваш дохід за поточний місяц: ')
+zminna2 = input('Введить суму регулярного платежу за кредтом: ')
+zminna3 = input('Введить поточну заборгованість за комунальні послуги: ')
+result1 = float(zminna1) - float(zminna2) - float(zminna3)
+result2= int(result1 *100)/100
+print ('Залишок вільних коштів складає :',result2,'грн.')"""
+
+
+
